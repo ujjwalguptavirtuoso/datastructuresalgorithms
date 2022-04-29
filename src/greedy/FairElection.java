75e16573fafea05d97990c26e2581189af45e392
wb-ujjwalgup584692 <wb-ujjwalgup584692@antfin.com>
2022-04-29 16:32:57 +0530

    greedy

---
+++ src/greedy/FairElection.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package recursion;
+
+/**
+ * @author wb-ujjwalgup584692
+ * @version $Id: FairElection.java, v 0.1 2022-04-29 12:35 wb-ujjwalgup584692 Exp $$
+ */
+public class FairElection {
+}
---
+++ src/recursion/PrintPattern.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package recursion;
+
+/**
+ * @author wb-ujjwalgup584692
+ * @version $Id: PrintPattern.java, v 0.1 2022-04-28 23:19 wb-ujjwalgup584692 Exp $$
+ */
+public class PrintPattern {
+}
